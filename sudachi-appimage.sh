35939cf2fd80d4ac71a24f1eb1e3223c2b9a11c4
TechDevangelist <97691709+TechDevangelist@users.noreply.github.com>
2025-04-07 07:42:43 -0400

    Update sudachi-appimage.sh

--- sudachi-appimage.sh
+++ sudachi-appimage.sh
@@ -48,8 +48,8 @@ cp setup.sh sudachi/
 	#find src -type f -name '*.cpp' -exec sed -i 's/boost::asio::io_service/boost::asio::io_context/g' {} \;
 
  	# Apply patches
-  	#unix2dos ../patches/fmt11-support.patch
-  	#patch -p1 -l --binary < ../patches/fmt11-support.patch
+  	unix2dos ../patches/fmt11-support.patch
+  	patch -p1 -l --binary < ../patches/fmt11-support.patch
 
    	#sed -i -e 's/FFmpeg 4.3 REQUIRED QUIET COMPONENTS/FFmpeg REQUIRED QUIET COMPONENTS/' CMakeLists.txt
    	 #sed -i -e 's/SDL_GetWindowProperties(window)/SDL_GetWindowProperties(render_window)/g' src/sudachi_cmd/emu_window/emu_window_sdl3_vk.cpp
